9ac5aa0fd50c2beb656c33dffc9484a98e91335b
Michael Nwogu <mikedbchi@gmail.com>
2020-12-16 23:07:17 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function() {
+    $( ".pass1" ).on( "click", function() {
+        alert("clicked");
+    });
+});
+
+$(function(){
+    $("#pass2").on("click", function()
+    {
+        alert("understood");
+    });
+});
